b1e80fb24fefb66604b34866f87f8e7656ce0493
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-04-07 20:13:58 -0500

    Guiados Cuadros 01

--- src/Ejercicio_01/CuadrosCombinados.java
+++ src/Ejercicio_01/CuadrosCombinados.java
@@ -8,12 +8,12 @@ package Ejercicio_01;
  *
  * @author Osvaldo
  */
-public class CuadrosDeLista extends javax.swing.JFrame {
+public class CuadrosCombinados extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form CuadrosCombinados
      */
-    public CuadrosDeLista() {
+    public CuadrosCombinados() {
         initComponents();
     }
 
@@ -27,43 +27,29 @@ public class CuadrosDeLista extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        btnAceptar = new javax.swing.JButton();
         etiResultado = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        lstColores = new javax.swing.JList<>();
+        cboColores = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setForeground(new java.awt.Color(0, 0, 0));
 
-        btnAceptar.setBackground(new java.awt.Color(255, 255, 255));
-        btnAceptar.setFont(new java.awt.Font("JetBrains Mono ExtraBold", 0, 14)); // NOI18N
-        btnAceptar.setForeground(new java.awt.Color(0, 0, 0));
-        btnAceptar.setText("Aceptar");
-        btnAceptar.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
-        btnAceptar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAceptarActionPerformed(evt);
-            }
-        });
-
-        etiResultado.setBackground(new java.awt.Color(255, 255, 255));
         etiResultado.setFont(new java.awt.Font("JetBrains Mono ExtraBold", 0, 18)); // NOI18N
         etiResultado.setForeground(new java.awt.Color(0, 0, 0));
+        etiResultado.setText("RESULTADO");
         etiResultado.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
 
-        lstColores.setBackground(new java.awt.Color(255, 255, 255));
-        lstColores.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
-        lstColores.setFont(new java.awt.Font("JetBrains Mono ExtraBold", 0, 14)); // NOI18N
-        lstColores.setForeground(new java.awt.Color(0, 0, 0));
-        lstColores.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Rojo", "Verde", "Azul" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
+        cboColores.setBackground(new java.awt.Color(255, 255, 255));
+        cboColores.setEditable(true);
+        cboColores.setFont(new java.awt.Font("JetBrains Mono ExtraBold", 0, 24)); // NOI18N
+        cboColores.setForeground(new java.awt.Color(0, 0, 0));
+        cboColores.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Rojo", "Verde", "Azul" }));
+        cboColores.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cboColoresActionPerformed(evt);
+            }
         });
-        lstColores.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jScrollPane1.setViewportView(lstColores);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -72,26 +58,21 @@ public class CuadrosDeLista extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(42, 42, 42)
-                        .addComponent(etiResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 456, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(157, 157, 157)
+                        .addComponent(etiResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(58, 58, 58)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(196, 196, 196)
-                        .addComponent(btnAceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(136, Short.MAX_VALUE))
+                        .addGap(98, 98, 98)
+                        .addComponent(cboColores, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(393, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(40, 40, 40)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
-                .addComponent(btnAceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(45, 45, 45)
-                .addComponent(etiResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18))
+                .addGap(179, 179, 179)
+                .addComponent(cboColores, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 295, Short.MAX_VALUE)
+                .addComponent(etiResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(153, 153, 153))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -108,18 +89,11 @@ public class CuadrosDeLista extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAceptarActionPerformed
-        String mensaje;
-
-        if (lstColores.getSelectedIndex() == -1) {
-            mensaje = "No hay un color seleccionado.";
-        } else {
-            mensaje = "El color seleccionado es: " + lstColores.getSelectedValue().toString();
-        }
+    private void cboColoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboColoresActionPerformed
+        String mensaje= "El color elegido es: ";
+        mensaje= mensaje+cboColores.getSelectedItem().toString();
         etiResultado.setText(mensaje);
-
-
-    }//GEN-LAST:event_btnAceptarActionPerformed
+    }//GEN-LAST:event_cboColoresActionPerformed
 
     /**
      * @param args the command line arguments
@@ -138,30 +112,27 @@ public class CuadrosDeLista extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CuadrosDeLista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CuadrosCombinados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CuadrosDeLista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CuadrosCombinados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CuadrosDeLista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CuadrosCombinados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CuadrosDeLista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CuadrosCombinados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CuadrosDeLista().setVisible(true);
+                new CuadrosCombinados().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAceptar;
+    private javax.swing.JComboBox<String> cboColores;
     private javax.swing.JLabel etiResultado;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JList<String> lstColores;
     // End of variables declaration//GEN-END:variables
 }
